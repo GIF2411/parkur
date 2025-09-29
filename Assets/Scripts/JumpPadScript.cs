610235222ae47d66ccf272854bcd42cf07eed6ee
GIF2411 <nitritoantico@gmail.com>
2025-09-29 18:27:13 +0200

    Update Fixed Bugs
    + Fixed jumppad, added SFX
    + Added more blocks
    - Removed broken music changes to water

---
+++ Assets/Scripts/JumpPadScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadScript : MonoBehaviour
+{
+    //script to add a force to every object that touches the main object, this script's parent
+
+    private Rigidbody[] toMove = null;
+    [SerializeField] private float forceAmount = 1f;
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField] private AudioSource jumpSoundSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toMove = GetComponentsInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toMove != null)
+        {
+            foreach (Rigidbody rb in toMove)
+            {
+                rb.AddForce(-transform.up * forceAmount);
+            }
+        }
+
+
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            toMove = contact.otherCollider.GetComponentsInChildren<Rigidbody>();
+        }
+        if (jumpSoundSource != null && jumpSound != null)
+        {
+            jumpSoundSource.PlayOneShot(jumpSound);
+        }
+
+    }
+
+
+
+    void OnCollisionExit(Collision collision)
+    {
+        toMove = null;
+    }
+
+}
